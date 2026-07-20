83d777afa94ac304976c12431d6a8e1877987bd0
sheila.cortesfernand <sheilacortesfdez@gmail.com>
2026-07-20 15:33:55 +0200

    updated catalog

--- js/catalog.js
+++ js/catalog.js
@@ -8,19 +8,19 @@
 var catalogData = [
   {
     id: 1,
-    name: "Demogorgon",
+    name: "Hollow Knight",
     category: "Fantasía",
-    price: 29.90,
-    stock: 6,
-    image: "img/demogorgon.jpeg",
-    description: { es: "Amigurumi detallado y aterrador, inspirado en Stranger Things, caracterizado por su cuerpo humanoide, alto y delgado, generalmente tejido en colores oscuros (negro/gris/marrón) y una cabeza distintiva en forma de flor roja de cinco pétalos.", en: "A detailed and terrifying amigurumi, inspired by Stranger Things, characterized by its tall and thin humanoid body, usually knitted in dark colors (black/grey/brown) and a distinctive head in the shape of a five-petaled red flower." }
+    price: 20.00,
+    stock: 1,
+    image: "img/elCaballeroHornet.jpeg",
+    description: { es: "Amigurumis de crochet hechos a mano inspirados en The Knight y Hornet de Hollow Knight. Cuidadosamente elaborados con materiales de calidad y gran atención al detalle, son ideales para coleccionistas, fans del videojuego, decoración o como un regalo único.", en: "Handmade crochet amigurumi inspired by The Knight and Hornet from Hollow Knight. Carefully crafted with quality materials and great attention to detail, they're perfect for collectors, fans of the game, home décor, or as a unique gift.." }
   },
   {
     id: 2,
     name: "Grinch",
     category: "Fantasía",
-    price: 31.50,
-    stock: 4,
+    price: 10.00,
+    stock: 1,
     image: "img/grinch.jpeg",
     description: { es: "Amigurumi navideño popular, tejido generalmente en verde con punto bajo, caracterizado por una expresión gruñona, ojos amarillos de seguridad, cejas bordadas y a menudo con traje rojo de fieltro o lana.", en: "Popular Christmas amigurumi, usually knitted in green with single crochet, characterized by a grumpy expression, yellow safety eyes, embroidered eyebrows and often with a red felt or wool suit." }
   },
@@ -28,8 +28,8 @@ var catalogData = [
     id: 3,
     name: "Novia Cadaver",
     category: "Fantasía",
-    price: 34.00,
-    stock: 3,
+    price: 25.00,
+    stock: 1,
     image: "img/noviaCadaver.jpeg",
     description: { es: "Amigurumi gótico-tierno, generalmente tejido con hilo de algodón en tonos azul pálido, blanco y gris. Caracterizada por un vestido de novia andrajoso, cabello largo azul, detalles bordados (ojos, cicatrices) y a menudo un velo de novia, se trabaja con técnicas básicas y puntos altos.", en: "A cute gothic amigurumi, usually crocheted with cotton yarn in pale blue, white, and gray tones. Characterized by a tattered wedding dress, long blue hair, embroidered details (eyes, scars), and often a bridal veil, it is made using basic techniques and double crochet stitches." }
   },
@@ -37,37 +37,55 @@ var catalogData = [
     id: 4,
     name: "Simba",
     category: "Animal",
-    price: 27.50,
-    stock: 8,
+    price: 30.00,
+    stock: 1,
     image: "img/simba.jpeg",
-    description: { es: "Peluche tejido a mano, generalmente con hilo de algodón color marrón-dorado y crema, caracterizado por su melena de hojas (inspirada en la canción 'Yo quisiera ya ser el rey') y detalles bordados.", en: "Hand-knitted plush toy, usually made with brown-gold and cream colored cotton yarn, characterized by its mane of leaves (inspired by the song 'I wish I were king') and embroidered details" }
+    description: { es: "Peluche tejido a mano, generalmente con hilo de algodón color marrón-dorado y crema", en: "Hand-knitted plush toy, usually made with brown-gold and cream colored cotton yarn" }
   },
   {
     id: 5,
     name: "Sony",
     category: "Fantasía",
-    price: 38.90,
-    stock: 2,
+    price: 30.00,
+    stock: 1,
     image: "img/sony.jpeg",
     description: { es: "Muñeco tejido a mano, generalmente diseñado con la técnica japonesa de amigurumi (tejido en espiral con puntos bajos), que representa al famoso erizo azul de los videojuegos.", en: "A hand-knitted doll, usually designed using the Japanese amigurumi technique (knitting in a spiral with single crochet stitches), representing the famous blue hedgehog from video games." }
   },
   {
     id: 6,
     name: "Tarta de Fresa",
-    category: "Fantasía",
-    price: 22.00,
-    stock: 10,
+    category: "Muñeca",
+    price: 20.00,
+    stock: 1,
     image: "img/tartaFresa.jpeg",
-    description: { es: "Se caracteriza por su cabello fucsia, vestido rojo, delantal blanco y sombrero con fresas, midiendo usualmente entre 20 cm y 36 cm, ideal para nivel intermedio.", en: "She is characterized by her fuchsia hair, red dress, white apron and hat with strawberries, usually measuring between 20 cm and 36 cm, ideal for intermediate level." }
+    description: { es: "Un dulce amigurumi con forma de tarta de fresa, tejido a mano con mucho cariño. Su diseño tierno y colorido lo convierte en un detalle perfecto para decorar cualquier rincón, regalar o añadir a una colección de amigurumis. Cada pieza está hecha artesanalmente, por lo que puede tener pequeños detalles que la hacen única.", en: "A sweet strawberry cake-shaped amigurumi, lovingly handmade. Its cute, colorful design makes it a perfect decorative accent, a lovely gift, or a great addition to an amigurumi collection. Each piece is handcrafted, so it may feature small details that make it unique." }
   },
      {
        id: 7,
        name: "Bebé Triceratops",
        category: "Animal",
-       price: 22.00,
-       stock: 10,
+       price: 15.00,
+       stock: 1,
        image: "img/pinkDino.jpeg",
        description: { es: "Bebé Triceratops de color rosa.", en: "Pink Baby Triceratops." }
+     },
+      {
+        id: 8,
+        name: "Dulce Primavera",
+        category: "Muñeca",
+        price: 25.00,
+        stock: 1,
+        image: "img/muñecaAzul.jpeg",
+        description: { es: "Muñeca amigurumi tejida a mano con crochet, de cabello rosa y un delicado conjunto en tonos pastel con sombrero y un pequeño bolsito de rana. Cada detalle está elaborado con cuidado, convirtiéndola en una pieza única, perfecta para decorar, coleccionar o regalar.", en: "Handmade crochet amigurumi doll with pink hair, wearing a charming pastel outfit, matching hat, and a cute little frog purse. Carefully crafted with attention to every detail, this one-of-a-kind doll is perfect for display, collecting, or as a thoughtful gift." }
+      },
+     {
+       id: 9,
+       name: "Ratoncita Hada Blanca",
+       category: "Animal",
+       price: 25.00,
+       stock: 1,
+       image: "img/sleepRat.jpeg",
+       description: { es: "Una encantadora ratoncita tejida a crochet con un delicado vestido blanco, gorrito puntiagudo con pompón y un simpático flequillo amarillo. Sus grandes orejas, mejillas sonrosadas y pequeña lengüita le dan una expresión dulce y divertida. Hecha a mano con la técnica amigurumi, es perfecta para decorar, regalar o formar parte de una colección de personajes tejidos.", en: "A charming crochet mouse featuring a delicate white dress, a pointed hat with a fluffy pom-pom, and cheerful yellow bangs. Her oversized ears, rosy cheeks, and tiny tongue create an adorable and playful expression. Handmade using the amigurumi technique, this cute doll is perfect for decoration, gifting, or adding to any crochet character collection." }
      }
 ];
 
